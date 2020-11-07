75f3881d9645d1e3adcb1a91d11f2d7887de63aa
Adriane Hairston <adrih1110@comcast.net>
2020-11-07 12:43:35 -0800

    user returns array of requested and given books

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,31 @@
 const SQL = require('sql-template-strings')
 const db = require('../lib/db')
 
-exports.getUsers = async function (req, res) {
+exports.getUsers = async function(req, res) {
   try {
-    const users = await db.query(SQL`SELECT * FROM user`)
+    const users = await db.query(SQL `SELECT * FROM user`)
     if (users.error) {
       return res.status(500).json(users.error)
     }
+    for (const user of users) {
+      user.givenBooks = await db.query(SQL `SELECT title, author, genre, description, year_published, publisher, image
+      FROM book
+      INNER JOIN owned_book ON book.book_id = owned_book.book_id
+      INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
+      WHERE swap.receiver_id IS NOT NULL AND owned_book.user_id = ${user.user_id}`)
+      if (user.givenBooks.error) {
+        return res.status(500).json(user.givenBooks.error)
+      }
+      user.receivedBooks = await db.query(SQL `SELECT title, author, genre, description, year_published, publisher, image
+        FROM book
+        INNER JOIN owned_book ON book.book_id = owned_book.book_id
+        INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
+        WHERE swap.receiver_id = ${user.user_id}`)
+      if (user.receivedBooks.error) {
+        return res.status(500).json(user.recievedBooks.error)
+      }
+    }
+
     return res.status(200).json(users)
   } catch (error) {
     console.log(error)
@@ -14,16 +33,34 @@ exports.getUsers = async function (req, res) {
   }
 }
 
-exports.getUser = async function (req, res) {
+exports.getUser = async function(req, res) {
   try {
     // Query should return an array with the user as the first and only
     // element. Destructure the array and store the element in 'user'.
-    const [user] = await db.query(SQL`SELECT * FROM user WHERE user_id = ${req.params.userId}`)
+    const [user] = await db.query(SQL `SELECT * FROM user WHERE user_id = ${req.params.userId}`)
     if (user.error) {
       return res.status(500).json(user.error)
     }
     if (!user) {
-      return res.status(404).json({ error: 'No user with this user_id exists' })
+      return res.status(404).json({
+        error: 'No user with this user_id exists'
+      })
+    }
+    user.givenBooks = await db.query(SQL `SELECT title, author, genre, description, year_published, publisher, image
+      FROM book
+      INNER JOIN owned_book ON book.book_id = owned_book.book_id
+      INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
+      WHERE swap.receiver_id IS NOT NULL AND owned_book.user_id = ${req.params.userId}`)
+    if (user.givenBooks.error) {
+      return res.status(500).json(user.givenBooks.error)
+    }
+    user.receivedBooks = await db.query(SQL `SELECT title, author, genre, description, year_published, publisher, image
+        FROM book
+        INNER JOIN owned_book ON book.book_id = owned_book.book_id
+        INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
+        WHERE swap.receiver_id = ${req.params.userId}`)
+    if (user.receivedBooks.error) {
+      return res.status(500).json(user.recievedBooks.error)
     }
     return res.status(200).json(user)
   } catch (error) {
@@ -32,22 +69,24 @@ exports.getUser = async function (req, res) {
   }
 }
 
-exports.createUser = async function (req, res) {
+exports.createUser = async function(req, res) {
   // Confirm required fields were passed.
   if (!req.body.user.name || !req.body.user.email) {
     return res
       .status(400)
-      .json({ error: 'The request object is missing at least one of the required attributes' })
+      .json({
+        error: 'The request object is missing at least one of the required attributes'
+      })
   }
   try {
     const response = await db.query(
-      SQL`INSERT INTO user (name, email, street, city, state, zip) VALUES (${req.body.user.name}, ${req.body.user.email}, ${req.body.user.street}, ${req.body.user.city}, ${req.body.user.state}, ${req.body.user.zip}) `
+      SQL `INSERT INTO user (name, email, street, city, state, zip) VALUES (${req.body.user.name}, ${req.body.user.email}, ${req.body.user.street}, ${req.body.user.city}, ${req.body.user.state}, ${req.body.user.zip}) `
     )
     if (response.error) {
       return res.status(500).json(response.error)
     }
     // Get and send the user that was just created.
-    const [user] = await db.query(SQL`SELECT * FROM user WHERE user_id = ${response.insertId}`)
+    const [user] = await db.query(SQL `SELECT * FROM user WHERE user_id = ${response.insertId}`)
     return res.status(201).json(user)
   } catch (error) {
     console.log(error)
@@ -55,15 +94,17 @@ exports.createUser = async function (req, res) {
   }
 }
 
-exports.editUser = async function (req, res) {
+exports.editUser = async function(req, res) {
   try {
-    const [user] = await db.query(SQL`SELECT * FROM user WHERE user_id = ${req.params.userId}`)
+    const [user] = await db.query(SQL `SELECT * FROM user WHERE user_id = ${req.params.userId}`)
     if (!user) {
-      return res.status(404).json({ error: 'No user with this user_id exists' })
+      return res.status(404).json({
+        error: 'No user with this user_id exists'
+      })
     }
     // Update the user with the values passed in the request if they exist.
     // FIXME: Add error handling to prevent user's points from reaching < 0
-    const response = await db.query(SQL`
+    const response = await db.query(SQL `
       UPDATE user
       SET name = ${req.body.user.name || user.name},
           email = ${req.body.user.email || user.email},
@@ -79,7 +120,7 @@ exports.editUser = async function (req, res) {
     }
     // Send the updated user.
     const [updatedUser] = await db.query(
-      SQL`SELECT * FROM user WHERE user_id = ${req.params.userId}`
+      SQL `SELECT * FROM user WHERE user_id = ${req.params.userId}`
     )
     return res.status(200).json(updatedUser)
   } catch (error) {}
