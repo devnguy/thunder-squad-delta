023c43d92d308894df6599df98ff866ee4b4161d
Adriane Hairston <adrih1110@comcast.net>
2020-11-15 21:34:00 -0800

    getUser route update to reflect profile page needs

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,32 +35,73 @@ exports.getUsers = async function (req, res) {
 
 exports.getUser = async function (req, res) {
   try {
-    // Query should return an array with the user as the first and only
-    // element. Destructure the array and store the element in 'user'.
-    const [user] = await db.query(SQL`SELECT * FROM user WHERE user_id = ${req.params.userId}`)
+    // Query returns information about user, swaps, and wishlist
+    // Response must be formatted to remove duplicate information
+    const user = await db.query(SQL`
+    SELECT b.book_id, b.title, b. author, b.genre, b.description, b.year_published, b.publisher, b.image,
+    w.wish_id, w.status, w.user_id,
+    s.swap_id, s.receiver_id, s.status, 
+    u.user_id, u.name, u.email, u.points, u.street, u.city, u.state, u.zip, u.points_spent,
+    (SELECT COUNT(swap.book_id) FROM swap
+      WHERE owner_id = ${req.params.userId} AND swap.status = 'complete') AS given_books,
+    (SELECT COUNT(swap.book_id) FROM swap
+      WHERE receiver_id = ${req.params.userId} AND swap.status = 'complete') AS received_books
+    FROM book AS b
+    LEFT JOIN wishlist AS w ON w.book_id = b.book_id
+    LEFT JOIN swap AS s ON s.book_id = b.book_id
+    JOIN user AS u ON s.owner_id = u.user_id OR w.user_id = u.user_id
+    WHERE w.user_id = ${req.params.userId} OR s.owner_id = ${req.params.userId} OR s.receiver_id = ${req.params.userId} OR u.user_id = ${req.params.userId};
+    `)
     if (user.error) {
       return res.status(500).json(user.error)
     }
     if (!user) {
       return res.status(404).json({ error: 'No user with this user_id exists' })
     }
-    user.givenBooks = await db.query(SQL`SELECT title, author, genre, description, year_published, publisher, image
-      FROM book
-      INNER JOIN owned_book ON book.book_id = owned_book.book_id
-      INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
-      WHERE swap.receiver_id IS NOT NULL AND owned_book.user_id = ${req.params.userId}`)
-    if (user.givenBooks.error) {
-      return res.status(500).json(user.givenBooks.error)
+    let formattedUser = {
+      userInfo: {},
+      library: [],
+      wishlist: []
     }
-    user.receivedBooks = await db.query(SQL`SELECT title, author, genre, description, year_published, publisher, image
-        FROM book
-        INNER JOIN owned_book ON book.book_id = owned_book.book_id
-        INNER JOIN swap ON owned_book.owned_book_id = swap.owned_book_id
-        WHERE swap.receiver_id = ${req.params.userId}`)
-    if (user.receivedBooks.error) {
-      return res.status(500).json(user.recievedBooks.error)
+    // Format user if it has at least one element (search was successful)
+    if (user.length > 0) {
+      formattedUser.userInfo.pointsSpent = user[0].points_spent
+      formattedUser.userInfo.pointsInWallet = user[0].points
+      formattedUser.userInfo.booksGiven = user[0].given_books
+      formattedUser.userInfo.booksReceived = user[0].received_books
+      // Iterate through each book returned in the user query and add it to the library or wishlist
+      user.forEach((book) => {
+        if (book.wish_id != null) {
+          formattedUser.wishlist.push(
+            {
+              book_id: book.book_id,
+              title: book.title,
+              author: book.author,
+              genre: book.genre,
+              description: book.description,
+              year_published: book.year_published,
+              publisher: book.publisher,
+              image: book.image
+            }
+          )
+        }
+        else if (book.swap_id != null && book.receiver_id == null) {
+          formattedUser.library.push(
+            {
+              book_id: book.book_id,
+              title: book.title,
+              author: book.author,
+              genre: book.genre,
+              description: book.description,
+              year_published: book.year_published,
+              publisher: book.publisher,
+              image: book.image
+            }
+          )
+        }
+      })
     }
-    return res.status(200).json(user)
+    return res.status(200).json(formattedUser)
   } catch (error) {
     console.log(error)
     return res.json(error)
