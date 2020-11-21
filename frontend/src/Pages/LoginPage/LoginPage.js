624b9a444891447be538138a7331c38c98f08896
Nathan <Nathan.shelby30@gmail.com>
2020-11-20 20:16:05 -0800

    Connecting Login

--- frontend/src/Pages/LoginPage/LoginPage.js
+++ frontend/src/Pages/LoginPage/LoginPage.js
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./LoginPage.css";
+import useApi from "../../Api/useApi";
+import requests from "../../Api/requests";
 import LoginIcon from "../../Assets/LoginIcon.png";
+import Routes from "../Routes";
 import { Link } from "react-router-dom";
 
-function InputField(props) {
-  return (
-    <input
-      className="field"
-      type="text"
-      placeholder={props.name}
-      id={props.name}
-      name={props.name}
-    />
-  );
-}
 
 function Login() {
+  const Login = useApi(requests.loginUser);
+  const[userName, setuserName] = React.useState("");
+  const[passWord, setpassWord] = React.useState("");
+
+  function handleUsername(event) {
+    setuserName(event.target.value);
+  }
+
+  
+  function handlepassWord(event) {
+    setpassWord(event.target.value);
+  }
+
+useEffect(()=>{
+  if (Login.data.status === true){
+    console.log(Login.data.id);
+  }  
+});
+
+function LoginUser(e) {
+    Login.request(userName, passWord);
+  }
+
+
   return (
     <div className="loginPage">
       <div id="regIcon">
@@ -25,14 +41,21 @@ function Login() {
           <div className="loginForm">
             <h2 className="title"> Login </h2>
             <div className="labels">
-              <label>
-                <InputField name="Username" />
-              </label>
+                <input 
+                className="field"
+                placeholder="Username"
+                type="text" 
+                name="Username" 
+                value={userName} 
+                onChange={handleUsername} 
+                />
               <input
                 type="password"
                 placeholder="Password"
                 name="password"
                 className="field"
+                value={passWord}
+                onChange={handlepassWord}
               />
             </div>
 			      <div id="links">
@@ -40,7 +63,7 @@ function Login() {
                 <Link to="/Registration">Sign Up</Link>
               </h4>
             </div>
-            <button id="LoginSubmit" className="field">Submit</button>
+            <button id="LoginSubmit" className="field" onClick={LoginUser}>Submit</button>
           </div>
         </div>
     </div>
