c0d5f011b864de0b6608e6a549b3176750288fdb
Devin Nguyen <hdn301@gmail.com>
2020-11-27 13:27:24 -0600

    get swaps by user id returns both owned and requested swaps

--- controllers/util/formatSwaps.js
+++ controllers/util/formatSwaps.js
@@ -5,11 +5,26 @@ function formatSwaps(swaps) {
     status: swap.status,
     cost: swap.cost,
     creation_date: swap.creation_date,
+    date_requested: swap.date_requested,
+    shipping_cost: 500,
     owner: {
       id: swap.owner_id,
       name: swap.owner_name,
+      street: swap.owner_street,
+      city: swap.owner_city,
+      state: swap.owner_state,
+      zip: swap.owner_zip,
     },
-    receiver: swap.receiver_id ? { id: swap.receiver_id, name: swap.receiver_name } : null,
+    receiver: swap.receiver_id
+      ? {
+          id: swap.receiver_id,
+          name: swap.receiver_name,
+          street: swap.receiver_street,
+          city: swap.receiver_city,
+          state: swap.receiver_state,
+          zip: swap.receiver_zip,
+        }
+      : null,
     book: {
       id: swap.book_id,
       title: swap.title,
